3f5ea85c28c4b91157593d9ccad3277645e251c9
Carrie Vrtis <cjvrtis@gmail.com>
2018-02-26 20:28:37 -0800

    Fixed whitespace everywhere. Thanks vscode.

--- app/lib/cvProject.js
+++ app/lib/cvProject.js
@@ -7,74 +7,74 @@ var path = require('path');
 
 module.exports.makeRouter = function(pageId, manifest, rootDir, bg, errorHandler) {
 
-    var router = express.Router();
+	var router = express.Router();
 
-    router.get("/", (req, res) => {
-        var activeCat = manifest.default;
-        var activeProj = manifest[activeCat].default;
-        res.render(path.resolve('views/project'), {
-            activeCat: activeCat,
-            activeProj: activeProj,
-            projectData: manifest[activeCat].projects[activeProj],
-            lang: req.langData,
-            manifest: manifest,
-            rootDir: rootDir,
-            activePage: pageId,
-            bg: bg
-        });
-    });
-    
-    router.get("/:category", (req, res) => {
-        var activeCat;
-        if(manifest[req.params.category]) {
-            activeCat = req.params.category;
-        }
-        else {
-            errorHandler(req, res, 404);
-            return;
-        }
-        var activeProj = manifest[activeCat].default;
-        res.render(path.resolve('views/project'), {
-            activeCat: activeCat,
-            activeProj: activeProj,
-            projectData: manifest[activeCat].projects[activeProj],
-            lang: req.langData,
-            manifest: manifest,
-        	rootDir: rootDir,
-            activePage: pageId,
-            bg: bg
-        });
-    });
+	router.get("/", (req, res) => {
+		var activeCat = manifest.default;
+		var activeProj = manifest[activeCat].default;
+		res.render(path.resolve('views/project'), {
+			activeCat: activeCat,
+			activeProj: activeProj,
+			projectData: manifest[activeCat].projects[activeProj],
+			lang: req.langData,
+			manifest: manifest,
+			rootDir: rootDir,
+			activePage: pageId,
+			bg: bg
+		});
+	});
 
-    router.get("/:category/:project", (req, res) => {
-        var activeCat;
-        if(manifest[req.params.category]) {
-            activeCat = req.params.category;
-        }
-        else {
-            errorHandler(req, res, 404);
-            return;
-        }
-        var activeProj;
-        if(manifest[activeCat].projects[req.params.project]) {
-            activeProj = req.params.project;
-        }
-        else {
-            errorHandler(req, res, 404);
-            return;
-        }
-        res.render(path.resolve('views/project'), { 
-            activeCat: activeCat,
-            activeProj: activeProj,
-            projectData: manifest[activeCat].projects[activeProj],
-            lang: req.langData,
-            manifest: manifest,
-            rootDir: rootDir,
-            activePage:pageId,
-            bg: bg 
-        });
-    });
+	router.get("/:category", (req, res) => {
+		var activeCat;
+		if(manifest[req.params.category]) {
+			activeCat = req.params.category;
+		}
+		else {
+			errorHandler(req, res, 404);
+			return;
+		}
+		var activeProj = manifest[activeCat].default;
+		res.render(path.resolve('views/project'), {
+			activeCat: activeCat,
+			activeProj: activeProj,
+			projectData: manifest[activeCat].projects[activeProj],
+			lang: req.langData,
+			manifest: manifest,
+			rootDir: rootDir,
+			activePage: pageId,
+			bg: bg
+		});
+	});
 
-    return router;
+	router.get("/:category/:project", (req, res) => {
+		var activeCat;
+		if(manifest[req.params.category]) {
+			activeCat = req.params.category;
+		}
+		else {
+			errorHandler(req, res, 404);
+			return;
+		}
+		var activeProj;
+		if(manifest[activeCat].projects[req.params.project]) {
+			activeProj = req.params.project;
+		}
+		else {
+			errorHandler(req, res, 404);
+			return;
+		}
+		res.render(path.resolve('views/project'), {
+			activeCat: activeCat,
+			activeProj: activeProj,
+			projectData: manifest[activeCat].projects[activeProj],
+			lang: req.langData,
+			manifest: manifest,
+			rootDir: rootDir,
+			activePage:pageId,
+			bg: bg
+		});
+	});
+
+	return router;
 
 };
--- app/lib/cvRouter.js
+++ app/lib/cvRouter.js
@@ -11,14 +11,14 @@ var langDefault = 'en';
 var dockerSecrets;
 var parsedSecrets = {};
 try {
-    dockerSecrets = require('docker-secrets');
-    for(var x of Object.keys(dockerSecrets)) {
-        parsedSecrets[x] = JSON.parse(dockerSecrets[x]);
-    }
+	dockerSecrets = require('docker-secrets');
+	for(var x of Object.keys(dockerSecrets)) {
+		parsedSecrets[x] = JSON.parse(dockerSecrets[x]);
+	}
 }
 catch(ex) {
-    dockerSecrets = null;
-    parsedSecrets = null;
+	dockerSecrets = null;
+	parsedSecrets = null;
 }
 var recaptcha = require('node-recaptcha2').Recaptcha;
 var bodyParser = require('body-parser');
@@ -33,203 +33,203 @@ var writingManifest = require('../static/media/writing/manifest.json');
 
 module.exports.makeRouter = function(langFile, langForce) {
 
-    var router = express.Router();
-    router.use(locale(langSupported, langDefault));
+	var router = express.Router();
+	router.use(locale(langSupported, langDefault));
 
-    var rootDir = (langForce ? '/' + langForce + '/' : '/');
+	var rootDir = (langForce ? '/' + langForce + '/' : '/');
 
-    router.use( (req, res, next) => {
-        if(langForce) {
-            req.langData = langFile[langForce];
-        }
-        else {
-            req.langData = langFile[req.locale];
-        }
-        next();
-    });
+	router.use( (req, res, next) => {
+		if(langForce) {
+			req.langData = langFile[langForce];
+		}
+		else {
+			req.langData = langFile[req.locale];
+		}
+		next();
+	});
 
-    router.use(bodyParser.json());
-    router.use(bodyParser.urlencoded({ extended: true })); 
-    
-    router.get("/", (req, res) => {
-        res.render(path.resolve('views/index'), {
-            lang: req.langData,
-            rootDir: rootDir,
-            activePage: '',
-            bg: '0'
-        });
-    });
-    
-    router.get("/about", (req, res) => {
-        res.render(path.resolve('views/about'), {
-            lang: req.langData,
-            rootDir: rootDir,
-            activePage: 'about',
-            bg: '1'
-        });
-    });
+	router.use(bodyParser.json());
+	router.use(bodyParser.urlencoded({ extended: true }));
 
-    router.get("/contact", (req, res) => {
-        var rc = (parsedSecrets ? new recaptcha(parsedSecrets.recaptcha.public, parsedSecrets.recaptcha.private) : null);
-        res.render(path.resolve('views/contact'), {
-            lang: req.langData,
-            rootDir: rootDir,
-            activePage: 'contact',
-            bg: '6',
-            rc: (rc ? rc.toHTML() : null)
-        });
-    });
+	router.get("/", (req, res) => {
+		res.render(path.resolve('views/index'), {
+			lang: req.langData,
+			rootDir: rootDir,
+			activePage: '',
+			bg: '0'
+		});
+	});
 
-    router.post("/contact", function(req, res) {
+	router.get("/about", (req, res) => {
+		res.render(path.resolve('views/about'), {
+			lang: req.langData,
+			rootDir: rootDir,
+			activePage: 'about',
+			bg: '1'
+		});
+	});
 
-        var usrData = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            company: req.body.company,
-            email: req.body.email,
-            subject: req.body.subject,
-            message: req.body.message,
-            sendCopy: req.body.sendCopy
-        }
+	router.get("/contact", (req, res) => {
+		var rc = (parsedSecrets ? new recaptcha(parsedSecrets.recaptcha.public, parsedSecrets.recaptcha.private) : null);
+		res.render(path.resolve('views/contact'), {
+			lang: req.langData,
+			rootDir: rootDir,
+			activePage: 'contact',
+			bg: '6',
+			rc: (rc ? rc.toHTML() : null)
+		});
+	});
 
-        var captchaData = {
-            remoteip: req.connection.remoteAddress,
-            response: req.body['g-recaptcha-response']
-        };
+	router.post("/contact", function(req, res) {
 
-        var rc = (parsedSecrets ? new recaptcha(parsedSecrets.recaptcha.public, parsedSecrets.recaptcha.private, captchaData) : null);
+		var usrData = {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			company: req.body.company,
+			email: req.body.email,
+			subject: req.body.subject,
+			message: req.body.message,
+			sendCopy: req.body.sendCopy
+		}
 
-        var baseResponseData = {
-            lang: req.langData,
-            rootDir: rootDir,
-            activePage: 'contact',
-            bg: '6',
-            rc: (rc ? rc.toHTML() : null),
-            usrData: usrData
-        };
+		var captchaData = {
+			remoteip: req.connection.remoteAddress,
+			response: req.body['g-recaptcha-response']
+		};
 
-        // This code is only used in dev environments in the abscence of secrets.
-        if(!rc) {
-            rc = {}
-            rc.verify = function(cb) {
-                if(typeof cb === 'function') {
-                    cb(true, false);
-                }
-            };
-        }
+		var rc = (parsedSecrets ? new recaptcha(parsedSecrets.recaptcha.public, parsedSecrets.recaptcha.private, captchaData) : null);
 
-        rc.verify( (success, err) => {
-            if (success) {
-                if(usrData.email) {
-                    var mailFirstName = (usrData.firstName ? usrData.firstName : '');
-                    var mailLastName = (usrData.firstLast ? usrData.LastName : '');
-                    var mailCompany = (usrData.company ? usrData.company : '');
-                    var mailSubject = (usrData.subject ? usrData.subject : 'No Subject');
-                    var mailMessage = (usrData.message ? usrData.message : 'No Message');
-                    
-                    var mailOptions = {
-                        from: '"Carrie Vrtis" <noreply@carrievrtis.com>',
-                        to: 'cjvrtis@gmail.com',
-                        subject: mailSubject,
-                        text: "CarrieVrtis.com Contact\n\nFrom: " + mailFirstName + " " + mailLastName + " < " + usrData.email + ">\n" + mailCompany + "\n\n" + mailSubject + '\n\n' + mailMessage,
-                        html: '<p>CarrieVrtis.com Contact</p><hr /><p><b>From: ' + mailFirstName + ' ' + mailLastName + ' &lt;' + usrData.email + '&gt;</b></p><p>' + mailCompany + '</p><p>' + mailSubject + '</p><p>' + mailMessage + '</p>'
-                    };
+		var baseResponseData = {
+			lang: req.langData,
+			rootDir: rootDir,
+			activePage: 'contact',
+			bg: '6',
+			rc: (rc ? rc.toHTML() : null),
+			usrData: usrData
+		};
 
-                    if(usrData.sendCopy) {
-                        mailOptions.to = mailOptions.to + ', ' + usrData.email;
-                    }     
-                    if(parsedSecrets) {
-                        var transporter = nodemailer.createTransport({
-                            host: 'carrievrtis.com',
-                            port: 25,
-                            secure: false,
-                            auth: {
-                                user: parsedSecrets.email.user,
-                                pass: parsedSecrets.email.password
-                            },
-                        });
+		// This code is only used in dev environments in the abscence of secrets.
+		if(!rc) {
+			rc = {}
+			rc.verify = function(cb) {
+				if(typeof cb === 'function') {
+					cb(true, false);
+				}
+			};
+		}
 
-                        transporter.sendMail(mailOptions, (error, info) => {
-                            if (error) {
-                                var responseData = baseResponseData;
-                                responseData.response = 'fail';
-                                res.render(path.resolve('views/contact'), responseData);
-                            }
-                            else {
-                                var responseData = baseResponseData;
-                                responseData.response = 'confirm';
-                                res.render(path.resolve('views/contact'), responseData);
-                            }
-                        });
-                    
-                    }
-                    else {
-                        var responseData = baseResponseData;
-                        responseData.response = 'fail';
-                        res.render(path.resolve('views/contact'), responseData);
-                        //res.setHeader('Content-Type', 'application/json');
-                        //res.send(JSON.stringify(mailOptions));
-                    }
-                }
-                else {
-                    var responseData = baseResponseData;
-                    responseData.response = 'email';
-                    res.render(path.resolve('views/contact'), responseData);
-                }
-            }
-            else {
-                var responseData = baseResponseData;
-                responseData.response = 'captcha';
-                res.render(path.resolve('views/contact'), responseData);
-            }
-        });
-    });
+		rc.verify( (success, err) => {
+			if (success) {
+				if(usrData.email) {
+					var mailFirstName = (usrData.firstName ? usrData.firstName : '');
+					var mailLastName = (usrData.firstLast ? usrData.LastName : '');
+					var mailCompany = (usrData.company ? usrData.company : '');
+					var mailSubject = (usrData.subject ? usrData.subject : 'No Subject');
+					var mailMessage = (usrData.message ? usrData.message : 'No Message');
 
-    /* Multipurpose Error Handler */
+					var mailOptions = {
+						from: '"Carrie Vrtis" <noreply@carrievrtis.com>',
+						to: 'cjvrtis@gmail.com',
+						subject: mailSubject,
+						text: "CarrieVrtis.com Contact\n\nFrom: " + mailFirstName + " " + mailLastName + " < " + usrData.email + ">\n" + mailCompany + "\n\n" + mailSubject + '\n\n' + mailMessage,
+						html: '<p>CarrieVrtis.com Contact</p><hr /><p><b>From: ' + mailFirstName + ' ' + mailLastName + ' &lt;' + usrData.email + '&gt;</b></p><p>' + mailCompany + '</p><p>' + mailSubject + '</p><p>' + mailMessage + '</p>'
+					};
 
-    var sendError = (req, res, err) => {
-        switch(err) {
-            case 404:
-                res.statusCode = 404;
-                res.render(path.resolve('views/error/error'), { 
-                    lang: req.langData,
-                    rootDir: rootDir,
-                    errorCode: res.statusCode.toString(),
-                    bg: '0'
-                });
-                break;
-            default:
-                res.statusCode = 500;
-                res.render(path.resolve('views/error/error'), { 
-                    lang: req.langData,
-                    rootDir: rootDir,
-                    errorCode: res.statusCode.toString(),
-                    bg: '0'
-                });
-                break;
-        }
-    }
+					if(usrData.sendCopy) {
+						mailOptions.to = mailOptions.to + ', ' + usrData.email;
+					}
+					if(parsedSecrets) {
+						var transporter = nodemailer.createTransport({
+							host: 'carrievrtis.com',
+							port: 25,
+							secure: false,
+							auth: {
+								user: parsedSecrets.email.user,
+								pass: parsedSecrets.email.password
+							},
+						});
 
-    /* Project Routers */
+						transporter.sendMail(mailOptions, (error, info) => {
+							if (error) {
+								var responseData = baseResponseData;
+								responseData.response = 'fail';
+								res.render(path.resolve('views/contact'), responseData);
+							}
+							else {
+								var responseData = baseResponseData;
+								responseData.response = 'confirm';
+								res.render(path.resolve('views/contact'), responseData);
+							}
+						});
 
-    var codeRouter = cvProject.makeRouter('code', codeManifest, rootDir, '2', sendError);
-    router.use('/code', codeRouter);
+					}
+					else {
+						var responseData = baseResponseData;
+						responseData.response = 'fail';
+						res.render(path.resolve('views/contact'), responseData);
+						//res.setHeader('Content-Type', 'application/json');
+						//res.send(JSON.stringify(mailOptions));
+					}
+				}
+				else {
+					var responseData = baseResponseData;
+					responseData.response = 'email';
+					res.render(path.resolve('views/contact'), responseData);
+				}
+			}
+			else {
+				var responseData = baseResponseData;
+				responseData.response = 'captcha';
+				res.render(path.resolve('views/contact'), responseData);
+			}
+		});
+	});
 
-    var designRouter = cvProject.makeRouter('design', designManifest, rootDir, '3', sendError);
-    router.use('/design', designRouter);
+	/* Multipurpose Error Handler */
 
-    var photoRouter = cvProject.makeRouter('photo', photoManifest, rootDir, '4', sendError);
-    router.use('/photo', photoRouter);
+	var sendError = (req, res, err) => {
+		switch(err) {
+			case 404:
+				res.statusCode = 404;
+				res.render(path.resolve('views/error/error'), {
+					lang: req.langData,
+					rootDir: rootDir,
+					errorCode: res.statusCode.toString(),
+					bg: '0'
+				});
+				break;
+			default:
+				res.statusCode = 500;
+				res.render(path.resolve('views/error/error'), {
+					lang: req.langData,
+					rootDir: rootDir,
+					errorCode: res.statusCode.toString(),
+					bg: '0'
+				});
+				break;
+		}
+	}
 
-    var writingRouter = cvProject.makeRouter('writing', writingManifest, rootDir, '5', sendError);
-    router.use('/writing', writingRouter);
+	/* Project Routers */
 
-    /* Generic 404 */
+	var codeRouter = cvProject.makeRouter('code', codeManifest, rootDir, '2', sendError);
+	router.use('/code', codeRouter);
 
-    router.get('*', (req, res) => {
-        sendError(req, res, 404);
-    });
+	var designRouter = cvProject.makeRouter('design', designManifest, rootDir, '3', sendError);
+	router.use('/design', designRouter);
 
-    return router;
+	var photoRouter = cvProject.makeRouter('photo', photoManifest, rootDir, '4', sendError);
+	router.use('/photo', photoRouter);
+
+	var writingRouter = cvProject.makeRouter('writing', writingManifest, rootDir, '5', sendError);
+	router.use('/writing', writingRouter);
+
+	/* Generic 404 */
+
+	router.get('*', (req, res) => {
+		sendError(req, res, 404);
+	});
+
+	return router;
 
 };
