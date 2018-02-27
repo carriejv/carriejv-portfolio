7b11140951c68255aa646c6e7839069fc1f49862
Carrie Vrtis <cjvrtis@gmail.com>
2018-02-26 20:27:29 -0800

    Fixed an error where exact matches to an available image size would return the next highest resolution instead.

--- app/lib/cvPhotos.js
+++ app/lib/cvPhotos.js
@@ -10,35 +10,35 @@ var availableWidths = [1920, 1440, 1200, 960, 768, 576, 256];
 
 router.get("/:width/:img", (req, res) => {
 
-    var reqWidth = parseInt(req.params.width);
+	var reqWidth = parseInt(req.params.width);
 
-    if(isNaN(reqWidth)) {
-        res.redirect('/' + availableWidths[0] + '/' + req.params.img);
-    }
-    
-    var bestMatch = availableWidths[0];
-    for(var x of availableWidths) {
-        if(Math.abs(reqWidth - bestMatch) > Math.abs(reqWidth - x) && x - reqWidth > 0) {
-            bestMatch = x;
-        }
-    }
+	if(isNaN(reqWidth)) {
+		res.redirect('/' + availableWidths[0] + '/' + req.params.img);
+	}
 
-    var cachePath = path.resolve('static/media/photo/photo-cache/', bestMatch.toString(), req.params.img);
+	var bestMatch = availableWidths[0];
+	for(var x of availableWidths) {
+		if(Math.abs(reqWidth - bestMatch) > Math.abs(reqWidth - x) && x - reqWidth >= 0) {
+			bestMatch = x;
+		}
+	}
 
-    // Cached image check
-    fs.access(cachePath, fs.constants.R_OK, (err) => {
-        if(!err) {
-            res.sendFile(cachePath, {maxAge: ms('1 month')}, (err) => {
-                if(err) {
-                    res.status(500).end();
-                }
-            });
-            return;
-        }
-        else {
-            res.sendStatus(404);
-        }
-    });
+	var cachePath = path.resolve('static/media/photo/photo-cache/', bestMatch.toString(), req.params.img);
+
+	// Cached image check
+	fs.access(cachePath, fs.constants.R_OK, (err) => {
+		if(!err) {
+			res.sendFile(cachePath, {maxAge: ms('1 month')}, (err) => {
+				if(err) {
+					res.status(500).end();
+				}
+			});
+			return;
+		}
+		else {
+			res.sendStatus(404);
+		}
+	});
 });
 
 module.exports = router;
