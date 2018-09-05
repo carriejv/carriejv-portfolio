129ff05c61584c2126c79844ff9abf7bd6d60929
Carrie <cjvrtis@gmail.com>
2018-09-04 19:51:48 -0700

    Added typing text homepage.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -2,21 +2,58 @@
 
 (function($) {
 
-	var index = 0;
-	var headersEN = [
-		'a digital media designer',
-		'a full-stack web developer',
-		'a photographer',
-		'a copywriter &amp; author',
-		'an online marketing expert'
-	];
+	var tgtObj = $('#typing-contents,#typing-mobile');
+	var textArr = window.subheaders;
+	var textIndex = 0;
+	var typingSpeed = 80;
+	var waitTime = 1000;
+	var isTyping = false;
+	var charIndex, textInterval, textTimeout;
 
-	var changeText = function(jqObj, headers) {
-		var removeLetter = function() {
-			if(jqObj.html() === '') {
-				addLetter();
+	var initText = function(jqObj) {
+		if(textInterval) {
+			window.clearInterval(textInterval);
+		};
+		if(textTimeout) {
+			window.clearInterval(textTimeout);
+		};
+		charIndex = jqObj.html().length - 1;
+		var tick = function() {
+			if(isTyping) {
+				jqObj.html(jqObj.html() + textArr[textIndex].charAt(charIndex));
+				if(charIndex === textArr[textIndex].length - 1) {
+					isTyping = false;
+					window.clearInterval(textInterval);
+					textTimeout = window.setTimeout(function() {
+						textInterval = window.setInterval(tick, typingSpeed);
+					}, waitTime);
+				}
+				else {
+					charIndex++;
+				}
 			}
-		}
+			else {
+				jqObj.html(jqObj.html().slice(0, -1));
+				if(charIndex === 0) {
+					textIndex++;
+					if(textIndex === textArr.length) {
+						textIndex = 0;
+					}
+					isTyping = true;
+				}
+				else {
+					charIndex--;
+				}
+			}
+		};
+		textTimeout = window.setTimeout(function() {
+			jqObj.parent().append('<span class="blink adark">_</span>');
+			textInterval = window.setInterval(tick, typingSpeed);
+		}, waitTime);
 	};
+
+	if(textArr) {
+		initText(tgtObj);
+	}
 
 })(jQuery);
